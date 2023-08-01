759675fda5676649ec4e524c963f61b1466ca24d
Jacek Pietruszka <pietruszka.jacek@gmail.com>
2023-08-01 11:16:31 +0200

    Clean 2

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,18 @@
-self.__NEXT_FONT_MANIFEST={pages:{"/_app":["static/media/b8ab9132ae10db55-s.p.woff2","static/media/b897bcc8a64c7158-s.p.woff2"]},app:{'/home/qsr/NODEDEV/kocotfoto/node_modules/next/font/google/target.css?{"path":"pages/_app.tsx","import":"Amatic_SC","arguments":[{"subsets":["latin"],"weight":["700"],"variable":"--font-amaticsc"}],"variableName":"amaticsc"}':["static/media/b8ab9132ae10db55-s.p.woff2"],'/home/qsr/NODEDEV/kocotfoto/node_modules/next/font/google/target.css?{"path":"pages/_app.tsx","import":"Dosis","arguments":[{"subsets":["latin"],"variable":"--font-dosis"}],"variableName":"dosis"}':["static/media/b897bcc8a64c7158-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!0};
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/_app": [
+      "static/media/b8ab9132ae10db55-s.p.woff2",
+      "static/media/b897bcc8a64c7158-s.p.woff2"
+    ]
+  },
+  "app": {
+    "/home/qsr/node/kocotfoto/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[1].oneOf[3].use[1]!/home/qsr/node/kocotfoto/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[1].oneOf[3].use[2]!/home/qsr/node/kocotfoto/node_modules/next/font/google/target.css?{\"path\":\"pages/_app.tsx\",\"import\":\"Amatic_SC\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"700\"],\"variable\":\"--font-amaticsc\"}],\"variableName\":\"amaticsc\"}": [
+      "static/media/b8ab9132ae10db55-s.p.woff2"
+    ],
+    "/home/qsr/node/kocotfoto/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[1].oneOf[3].use[1]!/home/qsr/node/kocotfoto/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[1].oneOf[3].use[2]!/home/qsr/node/kocotfoto/node_modules/next/font/google/target.css?{\"path\":\"pages/_app.tsx\",\"import\":\"Dosis\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-dosis\"}],\"variableName\":\"dosis\"}": [
+      "static/media/b897bcc8a64c7158-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": true
+}
--- .next/static/8uZ8UYe4ttUsTnEnPzFEW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/726-bc85fad6f39a6997.js","static/css/77df9106ed241c5d.css","static/chunks/pages/index-663df7ade43c8c4e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
