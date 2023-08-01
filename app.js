88fa2e2cad2b2e3ec96af65b639e734c7efd1247
Jacek Pietruszka <pietruszka.jacek@gmail.com>
2023-08-01 14:34:18 +0200

    Remove ignored files

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.prepare().then(() => {
     try {
       // Be sure to pass `true` as the second argument to `url.parse`.
       // This tells it to parse the query portion of the URL.
+      
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
  
