b55cc34fbd70d0bded342711de695e4c01807307
Jacek Pietruszka <pietruszka.jacek@gmail.com>
2023-08-01 14:41:49 +0200

    Test...

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="flex bg-center bg-no-repeat bg-cover h-screen bg-[url('/assets/header.webp')]">
       <Container>
         <div className='flex justify-center items-end h-full'>
-          <h2 className="font-amaticsc rounded-md bg-black/50 text-white text-5xl lg:text-7xl px-3 mb-[20vh]">Kamil Kocot Fotografia</h2>
+          <h2 className="font-amaticsc rounded-md bg-black/50 text-white text-5xl lg:text-7xl px-3 mb-[20vh]">Kamil Kocott Fotografia</h2>
         </div>
       </Container>
 
