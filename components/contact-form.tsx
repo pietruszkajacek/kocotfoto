51557ab1f97b80e7a5c5736a12aa6fb60d1fc9a7
Jacek Pietruszka <pietruszka.jacek@gmail.com>
2023-08-08 10:45:29 +0200

    contact-form transition

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -3,9 +3,9 @@ import { useCallback, useState, useRef } from "react";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 import ModalDialog from './modal-dialog';
 import classNames from 'classnames';
+import { InView } from 'react-intersection-observer';
 
 type InputState = "NotVerify" | "VerifyPass" | "VerifyNotPass";
-
 
 const ContactForm = () => {
   const { executeRecaptcha } = useGoogleReCaptcha();
@@ -66,48 +66,64 @@ const ContactForm = () => {
               <p className="py-5 lg:py-10">
                 Możesz się ze mną skontaktować na dwa sposoby — poprzez poniższy formularz lub korzystając z&nbsp;danych podanych na końcu tej strony.
               </p>
-              <form className="p-[30px] border-purple-mountains-majesty-600 rounded-3xl border mt-8 sm:w-2/3 mx-auto" method="POST" onSubmit={handleFormVerify}>
-                <div className="grid grid-cols-1 gap-6">
+              <InView triggerOnce={true}>
+                {({ inView, ref, entry }) => (
+                  <form
+                    ref={ref}
+                    className={classNames({
+                      'top-0': inView,
+                      'opacity-100': inView,
+                      'top-4': !inView,
+                      'opacity-0': !inView
+                    },
+                      "transition-all ease-in-out duration-500 delay-150 relative p-[30px] border-purple-mountains-majesty-600 rounded-3xl border mt-8 sm:w-2/3 mx-auto"
+                    )}
+                    method="POST"
+                    onSubmit={handleFormVerify}
+                  >
+                    <div className="grid grid-cols-1 gap-6">
 
-                  <label className="block">
-                    <span className="">Pole testowe</span>
-                    <input ref={inputRef} type="text" className={classNames(nameTest === 'VerifyNotPass' ? 'bg-rose-300' : '', "focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full")} placeholder="" />
-                  </label>
+                      <label className="block">
+                        <span className="">Pole testowe</span>
+                        <input ref={inputRef} type="text" className={classNames(nameTest === 'VerifyNotPass' ? 'bg-rose-300' : '', "focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full")} placeholder="" />
+                      </label>
 
-                  <label className="block">
-                    <span className="">Twoje imię i nazwisko</span>
-                    <input type="text" className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" placeholder="" />
-                  </label>
-                  <label className="block">
-                    <span className="">Twój email</span>
-                    <input type="email" className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" placeholder="twoj@email.pl" />
-                  </label>
-                  <label className="block">
-                    <span className="">Data ślubu / sesji</span>
-                    <input ref={dateRef} type="date" className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" />
-                  </label>
-                  <label className="block">
-                    <span className="">Jak do mnie trafiłaś / -eś</span>
-                    <select className="focus:border-fiolet focus:ring-fiolet text-black block w-full mt-1">
-                      <option>Internet (Facebook/Instagram/YouTube...)</option>
-                      <option>Znajomi</option>
-                      <option>Inny...</option>
-                    </select>
-                  </label>
-                  <label className="block">
-                    <span className="">Treść wiadomości</span>
-                    <textarea className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" rows={3}></textarea>
-                  </label>
-                  <div className="flex items-center justify-center">
-                    <button
-                      type="submit"
-                      className={"w-1/2 rounded-3xl bg-purple-mountains-majesty-600 uppercase font-medium px-3 py-2 shadow-sm hover:bg-purple-mountains-majesty-300 hover:text-purple-mountains-majesty-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-mountains-majesty-900"}
-                    >
-                      Wyślij
-                    </button>
-                  </div>
-                </div>
-              </form>
+                      <label className="block">
+                        <span className="">Twoje imię i nazwisko</span>
+                        <input type="text" className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" placeholder="" />
+                      </label>
+                      <label className="block">
+                        <span className="">Twój email</span>
+                        <input type="email" className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" placeholder="twoj@email.pl" />
+                      </label>
+                      <label className="block">
+                        <span className="">Data ślubu / sesji</span>
+                        <input ref={dateRef} type="date" className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" />
+                      </label>
+                      <label className="block">
+                        <span className="">Jak do mnie trafiłaś / -eś</span>
+                        <select className="focus:border-fiolet focus:ring-fiolet text-black block w-full mt-1">
+                          <option>Internet (Facebook/Instagram/YouTube...)</option>
+                          <option>Znajomi</option>
+                          <option>Inny...</option>
+                        </select>
+                      </label>
+                      <label className="block">
+                        <span className="">Treść wiadomości</span>
+                        <textarea className="focus:border-fiolet focus:ring-fiolet text-black mt-1 block w-full" rows={3}></textarea>
+                      </label>
+                      <div className="flex items-center justify-center">
+                        <button
+                          type="submit"
+                          className={"w-1/2 rounded-3xl bg-purple-mountains-majesty-600 uppercase font-medium px-3 py-2 shadow-sm hover:bg-purple-mountains-majesty-300 hover:text-purple-mountains-majesty-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-mountains-majesty-900"}
+                        >
+                          Wyślij
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                )}
+              </InView>
             </div>
           </div>
         </Container>
