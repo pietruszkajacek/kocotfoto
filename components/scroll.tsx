89e3078e00696ee6babffe414219ecbab392a42f
Jacek Pietruszka <pietruszka.jacek@gmail.com>
2023-08-31 12:34:56 +0200

    Scroll button responsive fix

--- components/scroll.tsx
+++ components/scroll.tsx
@@ -2,9 +2,8 @@ import styles from './scroll-styles.module.css'
 
 const Scroll = () => {
     return (
-        <button className={styles.scroll}>
+        <button className={`${styles.scroll} [--size:0.8] sm:[--size:1] md:[--size:1.2] lg:[--size:1.5]`}>
             <span>
-
             </span>
         </button>
     )
